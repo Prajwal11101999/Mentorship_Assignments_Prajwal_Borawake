1c14a9f0af0f2b5aaf737a3cecf197bb6e228d78
Prajwal11101999 <prajwal.borawake85@gmail.com>
2022-06-17 17:13:08 +0530

    Assignment3 WebApi

---
+++ Assignment 3 ProductUnitWebApi/ProductAssignment/Controllers/UnitcrudController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using ProductAssignment.Models;
+
+namespace ProductAssignment.Controllers
+{
+    public class UnitcrudController : Controller
+    {
+        // GET: Unitcrud
+        HttpClient client = new HttpClient();
+        ProductDetails1Entities db = new ProductDetails1Entities();
+
+
+        public ActionResult Index()
+        {
+            // List<UnitTB> Pro_list = new List<UnitTB>();
+            List<UnitTB> Pro_list = db.UnitTBs.ToList();
+            client.BaseAddress = new Uri("http://localhost:55898/Help/api/Get-api-Unitapi");
+            var response = client.GetAsync("Unitapi");
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                var display = test.Content.ReadAsAsync<List<UnitTB>>();
+                display.Wait();
+                Pro_list = display.Result;
+            }
+
+
+            return View(Pro_list);
+
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("")]
+        public ActionResult Create(UnitTB pro)
+        {
+
+
+            client.BaseAddress = new Uri("http://localhost:55898/api/get-api-Unitapi");
+            var response = client.PostAsJsonAsync<UnitTB>("Unitapi", pro);
+            response.Wait();
+
+            var test = response.Result;
+            if (test.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+
+            }
+
+            return View("Create");
+        }
+
+
+
+
+    }
+}
